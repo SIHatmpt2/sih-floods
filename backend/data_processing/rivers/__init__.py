bdb4cf602b70f7108ae050c53855a6f5942a515a
Saksham Kumar <sak289@outlook.com>
2026-09-01 14:47:33 +0530

    fix: avoid eager river pipeline import

--- backend/data_processing/rivers/__init__.py
+++ backend/data_processing/rivers/__init__.py
@@ -2,10 +2,9 @@
 
 from .clean import clean_cwc_data, normalize_cwc_columns
 from .features import add_river_features, select_xgboost_features
-from .pipeline import discover_csv_files, process_file, run_pipeline
 from .transform import transform_river_data
 
 __all__ = [
-    "add_river_features", "clean_cwc_data", "discover_csv_files", "normalize_cwc_columns",
-    "process_file", "run_pipeline", "select_xgboost_features", "transform_river_data",
+    "add_river_features", "clean_cwc_data", "normalize_cwc_columns",
+    "select_xgboost_features", "transform_river_data",
 ]
