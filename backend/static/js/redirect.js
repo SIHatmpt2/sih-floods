3ba2bd0bc90556945ded29542be915d5de6ec2ee
Saksham Kumar <sak289@outlook.com>
2026-09-11 23:59:53 +0530

    Fix redirect map tile provider

--- backend/static/js/redirect.js
+++ backend/static/js/redirect.js
@@ -22,9 +22,9 @@ function initMap() {
         scrollWheelZoom: true
     }).setView(center, 8);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
         maxZoom: 19,
-        attribution: "&copy; OpenStreetMap contributors"
+        attribution: "Tiles &copy; Esri"
     }).addTo(map);
 
     L.marker(center).addTo(map).bindPopup(selectedLocation.name).openPopup();
