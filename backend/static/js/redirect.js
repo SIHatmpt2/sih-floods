590cfc1c47c6c4471a0763be9208ee453d0f7810
VK-227 <vishal07.kumar222007@gmail.com>
2026-09-14 13:10:29 +0530

    feat: map historical analysis location

--- backend/static/js/redirect.js
+++ backend/static/js/redirect.js
@@ -1,13 +1,8 @@
 const locations = window.FLOODINTEL_LOCATIONS || {};
 const params = new URLSearchParams(window.location.search);
 const selectedKey = params.get("location") || "location1";
-const selectedLocation = locations[selectedKey] || Object.values(locations)[0] || null;
+const selectedLocation = window.FLOODINTEL_SELECTED_LOCATION || locations[selectedKey] || Object.values(locations)[0] || null;
 let map;
-
-const locationNameElement = document.getElementById("locationName");
-if (locationNameElement && selectedLocation) {
-    locationNameElement.textContent = selectedLocation.name;
-}
 
 function initMap() {
     const mapElement = document.getElementById("map");
@@ -20,9 +15,9 @@ function initMap() {
         zoomControl: false,
         attributionControl: true,
         scrollWheelZoom: true
-    }).setView(center, 8);
+    }).setView(center, 9);
 
-    L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
         maxZoom: 19,
         attribution: "Tiles &copy; Esri"
     }).addTo(map);
